702bd83e99d55db96a4943b609d35a6d0122f75e
0x4 <otavio.onoue@outlook.com>
2025-04-26 14:47:24 -0300

    chore: adjust the name of project

--- services/scraper.ts
+++ services/scraper.ts
@@ -9,7 +9,7 @@ export async function scrapeAmazon(keyword: string, page = "1"): Promise<Product
         const response = await axios.get(url, { 
             headers: {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-            } 
+            }
         });
 
         const dom = new JSDOM(response.data);
